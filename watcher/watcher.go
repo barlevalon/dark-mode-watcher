5881b06794e1c1f01da1be2d5938304241786cd1
Alon Hearter <alon.hearter@gmail.com>
2023-12-03 22:25:58 +0200

    remove log

--- watcher/watcher.go
+++ watcher/watcher.go
@@ -2,7 +2,6 @@ package watcher
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"os/exec"
 	"path"
@@ -18,22 +17,14 @@ func Watch() error {
 	commandString := fmt.Sprintf("defaults read %s/Library/Preferences/.GlobalPreferences.plist AppleInterfaceStyle", homeDir)
 	appearance := "init"
 	scriptPath := path.Join(homeDir, ".config/fish/functions/update_dark_mode.fish")
-	logFile, err := os.OpenFile(path.Join(homeDir, "dark-mode-watcher.log"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		return err
-	}
-	defer logFile.Close()
-	log.SetOutput(logFile)
-	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
 	for {
 		cmd := exec.Command("/bin/bash", "-c", commandString)
-		out, _ := cmd.CombinedOutput()
-		log.Println(string(out))
+		out, _ := cmd.Output()
 		temp := string(out)
 		if temp != appearance {
 			appearance = temp
-			out, _ := exec.Command(scriptPath, appearance).CombinedOutput()
-			log.Println(string(out))
+			exec.Command(scriptPath, appearance).Start()
 		}
 		time.Sleep(time.Second * 5)
 	}
